f53d535f91305a32785a8c99aab238da416c4d6f
mjzero0 <your-email@example.com>
2025-06-20 07:59:16 +0000

    [automatic commit] 2025-06-20 07:59:16 UTC 129.127.202.108

--- part1/db.js
+++ part1/db.js
@@ -29,3 +29,5 @@ db.query(`INSERT INTO Users (username, email, password_hash, role) VALUES
   ('alice123', 'alice@example.com', 'hashed123', 'owner'),
   ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
   ('carol123', 'carol@example.com', 'hashed789', 'owner')`);
+
+db.query('INSERT INTO Dogs')
