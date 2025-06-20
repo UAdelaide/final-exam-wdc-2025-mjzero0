6e506e3cceea76e5fc6b8e1aa74574e2b8c0d7f7
mjzero0 <your-email@example.com>
2025-06-20 08:00:58 +0000

    [automatic commit] 2025-06-20 08:00:58 UTC 129.127.202.108

--- part1/db.js
+++ part1/db.js
@@ -33,4 +33,4 @@ db.query(`INSERT INTO Users (username, email, password_hash, role) VALUES
 
   db.query(`INSERT INTO Dogs (owner_id, name, size) VALUES
   ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Max', 'medium'),
-  ((SELECT user_id FROM Users WHERE username = 'carol123'), 'Bella', 'small')`);
+  ((SELECT user_id FROM Users WHERE username = 'carol123'), 'Cat', 'small')`);
