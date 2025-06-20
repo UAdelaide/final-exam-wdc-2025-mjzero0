88f9870940722b218b79690227d1bcb232fd5341
mjzero0 <your-email@example.com>
2025-06-20 08:56:50 +0000

    [automatic commit] 2025-06-20 08:56:50 UTC 129.127.202.108

--- part1/routes/dogs.js
+++ part1/routes/dogs.js
@@ -7,11 +7,14 @@ const db = require('../db');
 router.get('/dogs', (req, res) => {
 
 
-const query = '
-SELECT d.name AS dog_name, d.size, u.username AS owner_username
+const query =
+'SELECT d.name AS dog_name, d.size, u.username AS owner_username
 FROM Dogs d
 JOIN Users u ON d.owner_id = u.user_id'
 
-
-
-}
+  `;
+  db.query(query, (err, results) => {
+    if (err) return res.status(500).json({ error: err.message });
+    res.json(results);
+  });
+});
