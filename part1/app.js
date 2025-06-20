5def3bf38e5f5edafc2f2f51a2eb63b88d56cba9
mjzero0 <your-email@example.com>
2025-06-20 09:29:32 +0000

    [automatic commit] 2025-06-20 09:29:32 UTC 129.127.202.108

--- part1/app.js
+++ part1/app.js
@@ -20,3 +20,12 @@ const PORT = 8080;
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
+
+
+
+//testing data
+db.query(`INSERT INTO Users (username, email, password_hash, role) VALUES
+  ('alice123', 'alice@example.com', 'hashed123', 'owner'),
+  ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
+  ('carol123', 'carol@example.com', 'hashed789', 'owner')
+`);
