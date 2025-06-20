2fa1de40c09abecb3602a9c81b7ce622b51c4b07
mjzero0 <your-email@example.com>
2025-06-20 11:28:36 +0000

    [automatic commit] 2025-06-20 11:28:36 UTC 129.127.202.108

--- part2/public/javascripts/page.js
+++ part2/public/javascripts/page.js
@@ -208,13 +208,11 @@ const email = document.getElementById('email').value;
 const password  = document.getElementById('password').value;
 fetch('api/users/login', {
 
+method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ email, password })
 
 
-
-
-
-
-    
 })
 
 }
