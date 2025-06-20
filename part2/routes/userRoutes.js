d32b2b6c3bf887333b232bf7b854f2d3deb6fe81
mjzero0 <your-email@example.com>
2025-06-20 11:50:08 +0000

    [automatic commit] 2025-06-20 11:50:08 UTC 129.127.202.108

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -48,6 +48,7 @@ router.post('/login', async (req, res) => {
     if (rows.length === 0) {
       return res.status(401).json({ error: 'Invalid credentials' });
     }
+    //just add this part
   req.session.user = rows[0];
     res.json({ message: 'Login successful', user: rows[0] });
   } catch (error) {
