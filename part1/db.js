b961e4005f5b079441eef283a90e74c61f49b691
mjzero0 <your-email@example.com>
2025-06-20 07:16:49 +0000

    [automatic commit] 2025-06-20 07:16:49 UTC 129.127.202.108

--- part1/db.js
+++ part1/db.js
@@ -1,13 +1,2 @@
 const mysql = require(mysql2/promise);
 const db = mysql.createPool ({
-
-host: 'localhost',
-user: 'root',
-password: '',
-database: 'DogWalkService'
-
-
-
-});
-
-module.exports = db;
