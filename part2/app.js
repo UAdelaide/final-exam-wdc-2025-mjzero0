46dd167f7ab59be35b830ca011c42a43bc60bc45
mjzero0 <your-email@example.com>
2025-06-20 14:37:53 +0000

    [automatic commit] 2025-06-20 14:37:53 UTC 129.127.202.108

--- part2/app.js
+++ part2/app.js
@@ -23,6 +23,10 @@ app.use(express.static(path.join(__dirname, '/public')));
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
 
+
+const dogsRoutes = require('./routes/dogsRoutes');
+
+
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
 
