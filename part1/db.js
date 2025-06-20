c8fe48bf289006fb5f5ae1b5b66c11f97e4805ba
mjzero0 <your-email@example.com>
2025-06-20 07:41:13 +0000

    [automatic commit] 2025-06-20 07:41:13 UTC 129.127.202.108

--- part1/db.js
+++ part1/db.js
@@ -3,4 +3,5 @@ const app = express();
 const db = require('./db');
 
 const dogsRoutes = require('./routes/dogs');
-const walkRequestsRoutes
+const walkRequestsRoutes = require ()
+const walkerRoutes
