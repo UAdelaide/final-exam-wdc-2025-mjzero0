e5b64d1e7cba4f8d2528363785c21a455df91aa2
mjzero0 <your-email@example.com>
2025-06-20 11:31:39 +0000

    [automatic commit] 2025-06-20 11:31:38 UTC 129.127.202.108

--- part2/public/javascripts/page.js
+++ part2/public/javascripts/page.js
@@ -212,7 +212,24 @@ method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ email, password })
 
-
+.then(res => res.json())
+.then(data => {
+  if (data && data.user) {
+    if (data.user.role == "owner") {
+      window.location = "owner-dashboard.html";
+    } else if (data.user.role == "walker") {
+      window.location = "walker-dashboard.html";
+    } else {
+      alert("no idea what role that is");
+    }
+  } else {
+    alert("wrong email or pass maybe");
+  }
+})
+.catch(err => {
+  console.log("something broke", err);
+  alert("server or smth is down");
+});
 })
 
 }
