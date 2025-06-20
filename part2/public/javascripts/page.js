19c4365e14cee75959770b518d1f92ee4c23056f
mjzero0 <your-email@example.com>
2025-06-20 11:25:12 +0000

    [automatic commit] 2025-06-20 11:25:12 UTC ;; communications error to 208.67.222.222#53: timed out 129.127.202.108

--- part2/public/javascripts/page.js
+++ part2/public/javascripts/page.js
@@ -174,32 +174,34 @@ function downvote(index) {
 }
 
 
-function login(){
+// function login(){
 
-    let user = {
-        user: document.getElementById('username').value,
-        pass: document.getElementById('password').value
-    };
+//     let user = {
+//         user: document.getElementById('username').value,
+//         pass: document.getElementById('password').value
+//     };
 
-    // Create AJAX Request
-    var xmlhttp = new XMLHttpRequest();
+//     // Create AJAX Request
+//     var xmlhttp = new XMLHttpRequest();
 
-    // Define function to run on response
-    xmlhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            alert("Welcome "+this.responseText);
-        } else if (this.readyState == 4 && this.status >= 400) {
-            alert("Login failed");
-        }
-    };
+//     // Define function to run on response
+//     xmlhttp.onreadystatechange = function() {
+//         if (this.readyState == 4 && this.status == 200) {
+//             alert("Welcome "+this.responseText);
+//         } else if (this.readyState == 4 && this.status >= 400) {
+//             alert("Login failed");
+//         }
+//     };
 
-    // Open connection to server & send the post data using a POST request
-    // We will cover POST requests in more detail in week 8
-    xmlhttp.open("POST", "/users/login", true);
-    xmlhttp.setRequestHeader("Content-type", "application/json");
-    xmlhttp.send(JSON.stringify(user));
+//     // Open connection to server & send the post data using a POST request
+//     // We will cover POST requests in more detail in week 8
+//     xmlhttp.open("POST", "/users/login", true);
+//     xmlhttp.setRequestHeader("Content-type", "application/json");
+//     xmlhttp.send(JSON.stringify(user));
 
-}
+// }
+
+//I'll write a new login fun
 
 function logout(){
 
