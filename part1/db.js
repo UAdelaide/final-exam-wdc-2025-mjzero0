a5a2314d6eb657d16b22ff6ce634c5b3de81cba6
mjzero0 <your-email@example.com>
2025-06-20 07:56:54 +0000

    [automatic commit] 2025-06-20 07:56:54 UTC 129.127.202.108

--- part1/db.js
+++ part1/db.js
@@ -25,4 +25,5 @@ db.query("DELETE FROM Users");
 
 //adding new data
 
-db.query('INSERT INTO Users (username, email, password_hash')
+db.query('INSERT INTO Users (username, email, password_hash, role) VALUES
+    ')
