2ff65cbb29d538d12474e71c73f2e64408f76d73
mjzero0 <your-email@example.com>
2025-06-20 09:01:35 +0000

    [automatic commit] 2025-06-20 09:01:35 UTC 129.127.202.108

--- part1/routes/walkrequests.js
+++ part1/routes/walkrequests.js
@@ -4,4 +4,19 @@ const db = require('../db');
 
 
 
-router.get()
+router.get('/walkrequests/open', (req, res) => {
+  const query = `
+    SELECT wr.request_id, d.name AS dog_name, wr.requested_time,
+           wr.duration_minutes, wr.location, u.username AS owner_username
+    FROM WalkRequests wr
+    JOIN Dogs d ON wr.dog_id = d.dog_id
+    JOIN Users u ON d.owner_id = u.user_id
+    WHERE wr.status = 'open'
+  `;
+  db.query(query, (err, results) => {
+    if (err) return res.status(500).json({ error: err.message });
+    res.json(results);
+  });
+});
+
+module
