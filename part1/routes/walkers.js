59436330187fbb0f5b1b6e8a5ab4c930a56a7a5a
mjzero0 <your-email@example.com>
2025-06-20 09:03:43 +0000

    [automatic commit] 2025-06-20 09:03:43 UTC ;; communications error to 208.67.222.222#53: timed out 129.127.202.108

--- part1/routes/walkers.js
+++ part1/routes/walkers.js
@@ -1,3 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../db');
+
+
+
+
+
+ db.query(query, (err, results) => {
+    if (err) return res.status(500).json({ error: err.message });
+    res.json(results);
+  });
+});
+
+module.exports = router;
