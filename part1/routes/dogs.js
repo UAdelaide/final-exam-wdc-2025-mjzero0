834a2a4906e73913348a7401a20e556985501ada
mjzero0 <your-email@example.com>
2025-06-20 08:53:26 +0000

    [automatic commit] 2025-06-20 08:53:26 UTC 129.127.202.108

--- part1/routes/dogs.js
+++ part1/routes/dogs.js
@@ -1,25 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-
-module.exports = (db) => {
-
-
-router.get('/dogs', async (req, res) => {
+const db = require('../db');
 
 
-    try {
-      const [rows] = await db.execute(`
-        SELECT Dogs.name AS dog_name, Dogs.size, Users.username AS owner_username
-        FROM Dogs
-        JOIN Users ON Dogs.owner_id = Users.user_id
-      `);
-      res.json(rows);
-    } catch (err) {
-      console.error('Error fetching dogs:', err);
-      res.status(500).json({ error: 'Failed to fetch dogs' });
-    }
-  });
-
-  return router;
-};
