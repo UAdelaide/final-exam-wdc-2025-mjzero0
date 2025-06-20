78d380e9271295d9c7aee0fe0592b7834fc0991b
mjzero0 <your-email@example.com>
2025-06-20 08:56:30 +0000

    [automatic commit] 2025-06-20 08:56:29 UTC 129.127.202.108

--- part1/routes/dogs.js
+++ part1/routes/dogs.js
@@ -10,13 +10,7 @@ router.get('/dogs', (req, res) => {
 const query = '
 SELECT d.name AS dog_name, d.size, u.username AS owner_username
 FROM Dogs d
-JOIN Users u ON d.owner_id = u.user_id
-
-
-
-
-
-'
+JOIN Users u ON d.owner_id = u.user_id'
 
 
 
